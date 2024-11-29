df1db82302b8f155efa201b642cc1ef1bcf77b5b
THMonster <5125031+THMonster@users.noreply.github.com>
2024-11-29 22:40:49 +0800

    123

--- src/dps.cpp
+++ src/dps.cpp
@@ -26,23 +26,17 @@ int64_t get_time_now() {
   return unix_timestamp;
 }
 
-// 函数：通过基址和多级偏移读取内存的值
 template <typename T> T read_memory(std::uintptr_t base_address, const std::vector<std::uintptr_t> &offsets) {
-  // plugin::log(loader::INFO, "read memory: {} {}", base_address, offsets);
-  // 当前地址
   auto current_address = *reinterpret_cast<std::uintptr_t *>(base_address);
 
-  // 遍历偏移链
   for (size_t i = 0; i < offsets.size(); ++i) {
     current_address += offsets[i];
 
-    // 如果不是最后一级偏移，将当前地址解释为指针继续解析
     if (i < offsets.size() - 1) {
       current_address = *reinterpret_cast<std::uintptr_t *>(current_address);
     }
   }
 
-  // 最终地址读取数据
   return *reinterpret_cast<T *>(current_address);
 }
 
@@ -114,7 +108,9 @@ std::string DPSMeter::get_dps_text() {
   auto now = get_time_now();
   auto total_damage = 0;
   for (auto m : members) {
-    total_damage = total_damage + m.damage;
+    if (m.state == 1) {
+      total_damage = total_damage + m.damage;
+    }
   }
   if (total_damage <= 0) {
     total_damage = 1;
@@ -122,7 +118,7 @@ std::string DPSMeter::get_dps_text() {
   for (auto m : members) {
     if (m.state == 1) {
       auto dps = (m.damage - m.start_damage) / (now - m.start_time);
-      float percent = (float)m.damage * 100 / total_damage ;
+      float percent = (float)m.damage * 100 / total_damage;
       ret.append(std::format("{}, {}dps, {}d, {:.1f}%\n", m.master_rank, dps, m.damage, percent));
     }
   }
